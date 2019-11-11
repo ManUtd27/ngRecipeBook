abfaa0f6015c787cacccb6416a7383a75e0a2def
Shawn Williams <jr.william09@gmail.com>
2019-11-11 12:49:49 -0600

    about to start working on the shopping list components

--- src/app/recipe-book/recipe-book.module.ts
+++ src/app/recipe-book/recipe-book.module.ts
@@ -6,13 +6,15 @@ import { RecipeBookComponent } from './recipe-book.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
+import {ClrDropdownModule} from '@clr/angular';
 
 
 @NgModule({
   declarations: [RecipeBookComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent],
   imports: [
     CommonModule,
-    RecipeBookRoutingModule
+    RecipeBookRoutingModule,
+    ClrDropdownModule
   ]
 })
 export class RecipeBookModule { }
