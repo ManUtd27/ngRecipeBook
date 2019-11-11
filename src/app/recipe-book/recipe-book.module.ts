3da8ff881f252e296f87eaed3848d8428f31be50
Shawn Williams <jr.william09@gmail.com>
2019-11-10 23:09:32 -0600

    created all components need thus far for each feature module

--- src/app/recipe-book/recipe-book.module.ts
+++ src/app/recipe-book/recipe-book.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { RecipeBookRoutingModule } from './recipe-book-routing.module';
 import { RecipeBookComponent } from './recipe-book.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 
 
 @NgModule({
-  declarations: [RecipeBookComponent],
+  declarations: [RecipeBookComponent, RecipeListComponent, RecipeDetailComponent, RecipeItemComponent],
   imports: [
     CommonModule,
     RecipeBookRoutingModule
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { ShoppingListRoutingModule } from './shopping-list-routing.module';
 import { ShoppingListComponent } from './shopping-list.component';
+import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
 
 
 @NgModule({
-  declarations: [ShoppingListComponent],
+  declarations: [ShoppingListComponent, ShoppingEditComponent],
   imports: [
     CommonModule,
     ShoppingListRoutingModule
