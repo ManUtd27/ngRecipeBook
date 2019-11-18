f0cb11e90f8a3c82dce3f632ba8af69aedb945be
Shawn Williams <jr.william09@gmail.com>
2019-11-17 22:30:40 -0600

    able to add ingredients from recipes to our shopping list via services

--- src/app/recipe-book/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe-book/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Recipe} from '../recipe';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,8 +9,12 @@ import {Recipe} from '../recipe';
 })
 export class RecipeDetailComponent implements OnInit {
   @Input() recipeItem: Recipe;
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+  }
+
+  addToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipeItem.ingredients);
   }
 }
--- src/app/recipe-book/recipe.service.ts
+++ src/app/recipe-book/recipe.service.ts
@@ -1,5 +1,7 @@
 import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe';
+import {Ingredient} from '../shared/ingredient';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,19 +10,39 @@ export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Salad', 3)
+      ]),
     new Recipe('BBQ', 'Summer Time Fun',
-      'https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false'),
+      'https://www.nps.gov/subjects/camping/images/recipe_1.jpg?maxwidth=1200&maxheight=1200&autorotate=false',
+      [
+        new Ingredient('Pork', 2),
+        new Ingredient('Peppers', 3)
+      ]),
     new Recipe('Dessert', 'For that sweet tooth',
-      'https://pixnio.com/free-images/2017/05/23/2017-05-23-17-26-29-900x428.jpg'
+      'https://pixnio.com/free-images/2017/05/23/2017-05-23-17-26-29-900x428.jpg',
+      [
+        new Ingredient('Milk', 1),
+        new Ingredient('Flower', 1),
+        new Ingredient('Eggs', 1)
+      ]
     ),
     new Recipe('A test Recipe', 'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Salad', 3)
+      ]),
   ];
 
-  constructor() {
+  constructor(private shoppingListService: ShoppingListService) {
   }
   getRecipes(): Recipe[] {
     return this.recipes.slice();
+  }
+  addIngredientsToShoppingList(ingredientsToAdd: Ingredient[]) {
+   this.shoppingListService.addIngredients(ingredientsToAdd);
   }
 }
