330cba001747412baaf4383024bfdb821c54d432
Shawn Williams <jr.william09@gmail.com>
2019-12-03 12:24:20 -0600

    working on edit recipe form

--- src/app/recipe-book/recipe-book.module.ts
+++ src/app/recipe-book/recipe-book.module.ts
@@ -6,7 +6,7 @@ import { RecipeBookComponent } from './recipe-book.component';
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
-import {ClrDropdownModule} from '@clr/angular';
+import {ClrDropdownModule, ClrFormsModule} from '@clr/angular';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
@@ -18,7 +18,8 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
     CommonModule,
     RecipeBookRoutingModule,
     ClrDropdownModule,
-    ScrollingModule
+    ScrollingModule,
+    ClrFormsModule
   ]
 })
 export class RecipeBookModule { }
