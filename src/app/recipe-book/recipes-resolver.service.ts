11af4f4579ad193ae22cbdae260efca5cc648c2b
Shawn Williams <jr.william09@gmail.com>
2019-12-16 14:48:28 -0600

    checked to see if we already have recipes in route resolver

--- src/app/recipe-book/recipes-resolver.service.ts
+++ src/app/recipe-book/recipes-resolver.service.ts
@@ -3,16 +3,21 @@ import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/rou
 import {Observable} from 'rxjs';
 import {Recipe} from './recipe';
 import {DataStorageService} from '../shared/data-storage.service';
+import {RecipeService} from './recipe.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecipesResolverService implements Resolve<Recipe[]> {
 
-  constructor(private dataStorageService: DataStorageService) { }
+  constructor(private dataStorageService: DataStorageService, private recipeService: RecipeService) { }
 
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Recipe[]> {
-    console.log('In Resolver');
-    return this.dataStorageService.fetchRecipes();
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Recipe[]> | Recipe[] {
+    const recipes = this.recipeService.getRecipes();
+    if (recipes.length === 0 ) {
+      return this.dataStorageService.fetchRecipes();
+    } else {
+      return recipes;
+    }
   }
 }
